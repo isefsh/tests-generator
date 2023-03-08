c4c5178dae628c555866a2cf1925275a26695628
Isabelle Hondo <isefshondo@gmail.com>
2023-03-07 23:43:38 -0300

    Fixed: DB questions and answers fixed

--- src/assets/functions/db.tsx
+++ src/assets/functions/db.tsx
@@ -174,9 +174,9 @@ export const db = {
       question:
         "Com relação à linguagem JavaScript, assinale a alternativa correta.",
       options: [
-        "A linguagem JavaScript exige muito trabalho para definir o tipo de dado que uma variável deve guardar.",
+        "Exige muito trabalho para definir o tipo de dado que uma variável deve guardar.",
         "Os números inteiros somente podem ser expressos na forma decimal (base 10).",
-        "Para que um código funcione corretamente na linguagem JavaScript, uma instrução if sempre deve ser associada a uma instrução else.",
+        "Para que funcione corretamente, uma instrução if sempre deve ser associada a uma instrução else.",
         "Em JavaScript, as expressões literais representam valores fixos."
       ],
       rightAnswer: 3,
@@ -214,7 +214,7 @@ export const db = {
     {
       abbrTheme: "Web",
       question:
-        "O padrão de descrição do formato de documentos XML que se constitui de instrumentos de descrição da estrutura destes documentos herdados da linguagem SGML e que tem como função fornecer meios de validar os dados XML e funcionar como uma gramática ou esquema para eles é denominado:",
+        "Documentos XML, herdados da linguagem SGML e que tem como função fornecer meios de validar os dados XML e funcionando como uma gramática ou esquema para eles é denominado:",
       options: [
         "Metadata",
         "XSLT",
@@ -228,7 +228,7 @@ export const db = {
     {
       abbrTheme: "Web",
       question:
-        "Assinale a alternativa que apresenta o nome da variável usada na linguagem de programação PHP que tem a função de armazenar dados do formulário e é considerada uma variável especial, conhecida também como superglobal, por ser interna ao PHP e ficar disponível para o script inteiro.",
+        "Variável usada na linguagem de programação PHP que tem a função de armazenar dados do formulário:",
       options: [
         "$_MSG",
         "$_POST",
@@ -270,12 +270,12 @@ export const db = {
     {
       abbrTheme: "Web",
       question:
-        "As linguagens de programação Web são utilizadas por programadores para o desenvolvimento Front-End (parte visual) e Back-End (estrutura que possibilita a operação) de sites, sistemas e aplicações Web em geral. Em relação às principais linguagens de programação Web, assinale a afirmativa correta.",
+        "Em relação às principais linguagens de programação Web, assinale a afirmativa correta.",
       options: [
-        "HTML: linguagem de marcação declarativa utilizada no desenvolvimento Back-End de sites dinâmicos.",
-        "PHP: linguagem interpretada de código aberto utilizada no desenvolvimento Back-End. Além disso, permite o desenvolvimento de sites estáticos.",
-        "JavaScript: linguagem de script que não precisa ser traduzida em código de máquina antes de ser executada, ou seja, executa de forma direta no código-fonte.",
-        "CSS: linguagem de estilo declarativa que determina como o código HTML deve ser exibido na tela. No entanto, não suporta que o estilo de uma página seja replicado a outras páginas."
+        "HTML: Utilizada no desenvolvimento Back-End de sites dinâmicos.",
+        "PHP: Utilizada no desenvolvimento Back-End. Além disso, permite o desenvolvimento de sites estáticos.",
+        "JavaScript: Não precisa ser traduzida em código de máquina antes de ser executada,  executa de forma direta no código-fonte.",
+        "CSS: Não suporta que o estilo de uma página seja replicado a outras páginas."
       ],
       rightAnswer: 2,
       userAnswer: null,
@@ -284,12 +284,12 @@ export const db = {
     {
       abbrTheme: "Web",
       question:
-        "Sobre a inclusão do conteúdo de um arquivo PHP em outro arquivo PHP (antes que o servidor o execute), é correto afirmar que:",
+        "Incluir um arquivo PHP em outro arquivo PHP (antes que o servidor o execute), é certo que:",
       options: [
-        "As instruções include e require são idênticas, isto é, produzem os mesmos resultados.",
-        "A instrução append é utilizada para incluir um arquivo de cabeçalho em todas as suas páginas web.",
-        "A instrução stop inclui um arquivo de chave para o fluxo de execução, evitando a integridade da aplicação.",
-        "O include produz um aviso e executa o script quando um arquivo é incluído com a instrução include e o PHP não consegue localizá-lo."
+        "as instruções include e require são idênticas, isto é, produzem os mesmos resultados.",
+        "a instrução append é utilizada para incluir um arquivo de cabeçalho em todas as suas páginas web.",
+        "a instrução stop inclui um arquivo de chave para o fluxo de execução, evitando a integridade da aplicação.",
+        "o include produz um aviso e executa o script quando um arquivo é incluído com a instrução include e o PHP não consegue localizá-lo."
       ],
       rightAnswer: 3,
       userAnswer: null,
@@ -326,14 +326,14 @@ export const db = {
     {
       abbrTheme: "Web",
       question:
-        "Assinale a opção que apresenta, se for o caso, a diferença entre os seletores CSS 'div > p' e 'div p'.",
+        "Ajax é um conjunto de tecnologias, suportadas por um navegador da web, tais como:  (1) XML, (2) CSS e (3) JavaScript. Da relação apresentadas:",
       options: [
-        '"div > p" seleciona todos os elementos <p> que são filhos diretos de um elemento <div>, enquanto "div p" seleciona todos os elementos <p> que são descendentes de um elemento <div> independentemente do nível de "nesting".',
-        '"div > p" seleciona todos os elementos <p> que são irmãos de um elemento <div>, enquanto "div p" seleciona todos os elementos <p> que são descendentes de um elemento <div> independentemente do nível de "nesting".',
-        '"div > p" seleciona todos os elementos <p> que são filhos diretos de um elemento <div>, enquanto "div p" seleciona todos os elementos <p> que são filhos diretos de um elemento <div> independentemente do nível de "nesting".',
-        '"div > p" seleciona todos os elementos <p> que são descendentes de um elemento <div>, enquanto "div p" seleciona todos os elementos <p> que são filhos diretos de um elemento <div> independentemente do nível de "nesting".'
+        "a)	existem somente o 1 e 2",
+        "b)	existem somente o 1 e 3 ",
+        "c)	existem somente o 2 e 3",
+        "d)	existem todos ",
       ],
-      rightAnswer: 0,
+      rightAnswer: 3,
       userAnswer: null,
       result: null
     },
@@ -382,7 +382,7 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "Os modelos tradicionais do ciclo de vida de um software consistem em formas de gerir as etapas de um projeto desde o momento de sua criação até sua eventual obsolescência. Por sua vez, esses ciclos podem ter suas fases identificadas pela presença de quatro elementos, são eles:",
+        "Os modelos tradicionais do ciclo de vida de um software possuem suas fases identificadas pela presença de quatro elementos, são eles:",
       options: [
         "a) Requisitos necessários, prototipagem, feedback.",
         "b) Requisitos necessários, produtos resultantes, prototipagem do sistema, resposta do usuário.",
@@ -396,7 +396,7 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "O mercado está lotado de modelos de ciclo de vida para suprir as necessidades atuais das empresas de software, os quais sempre se renovam juntamente às tecnologias. Assinale abaixo a alternativa que possui apenas modelos de ciclo de vida:",
+        "Existem diversos ciclos de vida para suprem as necessidades atuais das empresas. Assinale abaixo a alternativa que possui apenas modelos de ciclo de vida:",
       options: [
         "a)	Codifica-remenda, cascata, prototipação, iterativo, incremental, espiral.",
         "b)	Codifica-remenda, cascata, prototipação, interativo, incremental, espiral.",
@@ -424,7 +424,7 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "I) ______ é considerado o modelo de vida mais antigo, caracterizado por seu jeito suas etapas seguidas ordenadamente uma após a outra, as quais precisam ser reiniciadas desde o princípio caso haja uma mudança de requisitos. <br /> II) O modelo ______ é o mais caótico entre os demais, uma vez que pode não apresentar especificação de requisitos, o que lhe faz ser considerado de alto risco. <br /> III) O ______ é um modelo que visa o planejamento de retrabalho, no qual o tempo de revisão e melhorias do sistema é pré definido.",
+        "I) ______ é considerado o modelo de vida mais antigo; etapas reiniciadas desde o princípio caso haja uma mudança de requisitos. II) O modelo ______ é o mais caótico entre os demais. III) O ______ é um modelo que visa o planejamento de retrabalho, no qual o tempo de revisão e melhorias do sistema é pré-definido. Complete as lacunas vazias com os nomes dos modelos em sua respectiva ordem:",
       options: [
         "a)	Iterativo, prototipação, cascata.",
         "b)	Cascata, prototipação, espiral.",
@@ -438,7 +438,7 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "Sobre suas etapas, identifique as afirmações como verdadeiras ou falsas e assinale a alternativa que corresponde a tal. Engenharia de sistemas corresponde a fase de levantamento de requisitos referentes ao sistema. O projeto se refere a representação das funções do sistema e sua estrutura de dados, arquitetura de software, detalhes procedimentais e caracterização de interface. Manutenção se refere ao período de tempo no qual o sistema se torna obsoleto e é desativado permanentemente. A análise consiste na investigação da disponibilidade dos recursos e tecnologias para o sistema requisitado.",
+        "Em relação as etapas do modelo cascata, identifique as afirmações como verdadeiras ou falsas e assinale a alternativa correta: (  ) Engenharia de sistemas: Fase de levantamento de requisitos. (  ) Fase projeto: Representação das funções do sistema e sua estrutura de dados, arquitetura de software, detalhes procedimentais e caracterização de interface. (  ) Manutenção: Período de tempo no qual o sistema se torna obsoleto e é desativado permanentemente. (  ) A análise: Investigação da disponibilidade dos recursos e tecnologias.",
       options: [
         "a)	V, F, F, V.",
         "b)	V, V, F, F.",
@@ -452,12 +452,26 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "A prototipação é um modelo de desenvolvimento de software capaz de auxiliar na visualização de uma proposta dentro do projeto antes de implementá-la. Em relação a esse modelo, assinale a alternativa correta:",
+        "Assinale as alternativas corretas de acordo com a veracidade ou não das frases abaixo: I.	É correto planejar apenas uma fase e planejar a próxima somente quando o trabalho estiver avançando na fase atual e nas suas entregas. II.	Os modelos de ciclo de vida garantem que o sistema atenda aos requisitos desejados e funciona como esperado. III.	Requisitos de software nunca se modificam após serem definidos e documentados.",
+      options: [
+        "a)	V, F, V.",
+        "b)	V, V, V.",
+        "c)	F, F, V.",
+        "d)	V, V, F."
+      ],
+      rightAnswer: 3,
+      userAnswer: null,
+      result: null
+    },
+    {
+      abbrTheme: "GAPS",
+      question:
+        "Sobre a fase de prototipação, assinale a alternativa correta:",
       options: [
         "a)	Possui sete etapas bem separadas, as quais fluem entre si.",
-        "b)	Concentra-se em documentar os requisitos e os analisar cuidadosamente antes de iniciar projetos. ",
+        "b)	Documenta os requisitos e os analisar cuidadosamente antes de iniciar projetos. ",
         "c)	Ajuda o usuário a compreender os requisitos através da demonstração em experimentos.",
-        "d)	Precisa sempre satisfazer todos os requisitos do produto final em todas as etapas."
+        "d)	Satisfaz todos os requisitos do produto final em todas as etapas."
       ],
       rightAnswer: 2,
       userAnswer: null,
@@ -466,12 +480,26 @@ export const db = {
     {
       abbrTheme: "GAPS",
       question:
-        "De acordo com a 5ª edição do PMBOK, o ciclo de vida do ________ é a série de _______ pelas quais um projeto passa, desde o seu início até ___________. Assinale a alternativa que preenche, correta e respectivamente, as lacunas do trecho acima:",
+        "De acordo com a 5ª edição do PMBOK, o ciclo de vida do ________ é a série de _______ pelas quais um projeto passa, desde o seu início até ___________.",
       options: [
         "a)	produto – fases – a sua retirada.",
         "b)	produto – processos – o seu término.",
         "c)	projeto – processos – o seu fim.",
         "d)	projeto – fases – o seu término."
+      ],
+      rightAnswer: 3,
+      userAnswer: null,
+      result: null
+    },
+    {
+      abbrTheme: "GAPS",
+      question:
+        "A respeito dos diversos modelos de ciclo de vida do software, analise as afirmativas abaixo. I.  Cascata, evolutivo, incremental e espiral são exemplos de modelos de ciclo de vida de software. II. O modelo cascata é um modelo tradicional onde as fases são realizadas de forma serial em um processo que flui sempre para frente. III. O modelo incremental preconiza a divisão das entregas do projeto, característica que justifica seu nome. É correto o que se afirma:",
+      options: [
+        "a)	apenas em I.",
+        "b)	apenas em I e II.",
+        "c)	apenas em II e III.",
+        "d)	em I, II e III."
       ],
       rightAnswer: 3,
       userAnswer: null,
@@ -498,7 +526,7 @@ export const db = {
       options: [
         "a)	Para atender as exigências do mercado e aumentar a qualidade e agilidade durante a produção dos projetos.",
         "b)	Para gerenciar projetos, diminuir a agilidade e aumentar a qualidade dos projetos.",
-        "c)	A fim de diminuir a concorrência no mercado, tornando os projetos únicos, inclusive em seus modelos de produção.            ",
+        "c)	A fim de diminuir a concorrência no mercado, tornando os projetos únicos, inclusive em seus modelos de produção.",
         "d)	A fim de atrapalhar as grandes empresas e desorganizar a produção dos projetos."
       ],
       rightAnswer: 0,
@@ -538,10 +566,10 @@ export const db = {
       question:
         "Qual das alternativas abaixo corresponde a um modelo de desenvolvimento de software linear e sequencial?",
       options: [
-        "Espiral",
-        "Incremental",
-        "Codifica-remenda",
-        "Cascata"
+        "a)	Espiral.",
+        "b)	Incremental.",
+        "c)	Codifica-rementa.",
+        "d)	Cascata."
       ],
       rightAnswer: 3,
       userAnswer: null,
@@ -558,34 +586,6 @@ export const db = {
         "d)	Ideal para projetos com requisitos bem definidos."
       ],
       rightAnswer: 3,
-      userAnswer: null,
-      result: null
-    },
-    {
-      abbrTheme: "GAPS",
-      question:
-        "Gibberish",
-      options: [
-        "Gibberish",
-        "Gibberish",
-        "Gibberish",
-        "Gibberish"
-      ],
-      rightAnswer: 2,
-      userAnswer: null,
-      result: null
-    },
-    {
-      abbrTheme: "GAPS",
-      question:
-        "Gibberish",
-      options: [
-        "Gibberish",
-        "Gibberish",
-        "Gibberish",
-        "Gibberish"
-      ],
-      rightAnswer: 2,
       userAnswer: null,
       result: null
     },
